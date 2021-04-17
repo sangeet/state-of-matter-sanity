6800183b61922f76b9a6937d51040bf1f7758e00
Sangeet Khatri <sangeet.khatri@protonmail.com>
2021-04-17 11:07:57 +0530

    Added preview to issue and post, added dist to gitignore

--- schemas/issue.js
+++ schemas/issue.js
@@ -1,34 +1,45 @@
 export default {
-  title: 'Issues',
-  name: 'issues',
-  type: 'document',
+  title: "Issues",
+  name: "issues",
+  type: "document",
   fields: [
     {
-      title: 'Number',
-      name: 'number',
-      type: 'number',
+      title: "Number",
+      name: "number",
+      type: "number",
     },
     {
-      title: 'Posts',
-      name: 'posts',
-      type: 'array',
+      title: "Posts",
+      name: "posts",
+      type: "array",
       of: [
         {
-          type: 'reference',
-          to: [{ type: 'post' }],
+          type: "reference",
+          to: [{ type: "post" }],
         },
       ],
     },
     {
-      title: 'Editor',
-      name: 'editor',
-      type: 'array',
+      title: "Editor",
+      name: "editor",
+      type: "array",
       of: [
         {
-          type: 'reference',
-          to: [{ type: 'profile' }],
+          type: "reference",
+          to: [{ type: "profile" }],
         },
       ],
     },
   ],
+  preview: {
+    select: {
+      number: "number",
+    },
+    prepare(selection) {
+      const { number } = selection;
+      return {
+        title: `Issue - ${number}`,
+      };
+    },
+  },
 };
--- schemas/post.js
+++ schemas/post.js
@@ -1,74 +1,88 @@
 export default {
-  title: 'Post',
-  name: 'post',
-  type: 'document',
+  title: "Post",
+  name: "post",
+  type: "document",
   fields: [
     {
-      title: 'Title',
-      name: 'title',
-      type: 'string',
+      title: "Title",
+      name: "title",
+      type: "string",
     },
     {
-      title: 'Date',
-      name: 'date',
-      type: 'date',
+      title: "Slug",
+      name: "slug",
+      type: "slug",
+      validation: (Rule) => Rule.required(),
+      options: {
+        source: "title",
+      },
     },
     {
-      title: 'Body',
-      name: 'body',
-      type: 'array',
-      of: [{ type: 'block' }],
+      title: "Date",
+      name: "date",
+      type: "date",
     },
     {
-      title: 'Tags',
-      name: 'tags',
-      type: 'array',
+      title: "Body",
+      name: "body",
+      type: "array",
+      of: [{ type: "block" }],
+    },
+    {
+      title: "Tags",
+      name: "tags",
+      type: "array",
       of: [
         {
-          type: 'reference',
-          to: [{ type: 'tag' }],
+          type: "reference",
+          to: [{ type: "tag" }],
         },
       ],
     },
     {
-      title: 'Category',
-      name: 'category',
-      type: 'array',
+      title: "Category",
+      name: "category",
+      type: "array",
       of: [
         {
-          type: 'reference',
-          to: [{ type: 'category' }],
+          type: "reference",
+          to: [{ type: "category" }],
         },
       ],
     },
     {
-      title: 'Author',
-      name: 'author',
-      type: 'array',
+      title: "Author",
+      name: "author",
+      type: "array",
       of: [
         {
-          type: 'reference',
-          to: [{ type: 'profile' }],
+          type: "reference",
+          to: [{ type: "profile" }],
         },
       ],
     },
     {
-      title: 'Translated By',
-      name: 'translator',
-      type: 'array',
+      title: "Translated By",
+      name: "translator",
+      type: "array",
       of: [
         {
-          type: 'reference',
-          to: [{ type: 'profile' }],
+          type: "reference",
+          to: [{ type: "profile" }],
         },
       ],
     },
     {
-      title: 'Link To Podcast',
-      name: 'podcastLink',
-      type: 'url',
+      title: "Link To Podcast",
+      name: "podcastLink",
+      type: "url",
     },
   ],
+  preview: {
+    select: {
+      title: "title",
+    },
+  },
 };
 
 //Ability to create new tags directly from the list, if possible
