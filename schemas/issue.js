dcc90b4b5523aaa777e79fc17cf4c35e1c760a68
Sangeet Khatri <sangeet.khatri@protonmail.com>
2021-05-12 10:54:50 +0530

    Added year and month to issue

--- schemas/issue.js
+++ schemas/issue.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Link } from 'part:@sanity/base/router';
+import React from "react";
+import { Link } from "part:@sanity/base/router";
 
 export default {
   title: "Issues",
@@ -12,13 +12,46 @@ export default {
       type: "number",
     },
     {
+      title: "Month",
+      name: "month",
+      type: "string",
+      options: {
+        list: [
+          { title: "January", value: "1" },
+          { title: "February", value: "2" },
+          { title: "March", value: "3" },
+          { title: "April", value: "4" },
+          { title: "May", value: "5" },
+          { title: "June", value: "6" },
+          { title: "July", value: "7" },
+          { title: "August", value: "8" },
+          { title: "September", value: "9" },
+          { title: "October", value: "10" },
+          { title: "November", value: "11" },
+          { title: "December", value: "12" },
+        ],
+      },
+    },
+    {
+      title: "Year",
+      name: "year",
+      type: "number",
+      options: {
+        list: Array(new Date().getFullYear() - 2021 + 1)
+          .fill()
+          .map((_, i) => 2021 + i)
+      },
+    },
+    {
       title: "Posts",
       name: "posts",
       type: "array",
       description: (
         <span>
           Post to be added here must be created first in the{" "}
-          <Link href={"/intent/create/type=post;template=post/"}>Post List</Link>
+          <Link href={"/intent/create/type=post;template=post/"}>
+            Post List
+          </Link>
         </span>
       ),
       of: [
@@ -35,7 +68,9 @@ export default {
       description: (
         <span>
           Profile to be added here must be created first in the{" "}
-          <Link href={"/intent/create/type=profile;template=profile/"}>Profile List</Link>
+          <Link href={"/intent/create/type=profile;template=profile/"}>
+            Profile List
+          </Link>
         </span>
       ),
       of: [
